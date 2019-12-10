55aeaa44526cab841d207b6c1404da238abb890c
prashantgaur985 <58035181+prashantgaur985@users.noreply.github.com>
2019-12-10 20:32:34 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,7 @@ public class HelloWorld {
     System.out.println("Hello, World");
     System.out.println("Hello, Devops Class");
     System.out.println("Hello, Hello All");
+    System.out.println("Hello, My Friends");
+    System.out.println("Hello, All Guyz from Hyderabad");
   }
 }
